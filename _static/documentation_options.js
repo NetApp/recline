2fa354931c812f29da5f50e0d32b1bf9833a0d98
RobertBlackhart <robert@blackhartfamily.com>
2024-07-31 14:58:09 +0000

    Deploying to gh-pages from  @ 3eb13baa125fa6914c32f49afe24f576f88ca43f 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.6',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
